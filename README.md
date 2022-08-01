804528dfa8affa982ec1d91e9ee78e7726edac3d
Gabriele Franch <franch@gmail.com>
2022-08-01 11:00:24 +0000

    Add deployment settings for flask

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Live data fetching
 
-Run with: `FLASK_ENV="development" FLASKSK_DEBUG=True PG_CONNSTRING=postgresql://postgres:postgres@localhost:5432/webvalley2022 FLASK_APP=app flask run`
+Production: `PG_CONNSTRING=postgresql://postgres:postgres@localhost:5432/webvalley2022 FLASK_ENV="development" FLASKSK_DEBUG=True FLASK_APP=app flask run`
+
+Debug mode: `PG_CONNSTRING=postgresql://postgres:postgres@localhost:5432/webvalley2022 python app.py`
--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import json
-from datetime import datetime
-from flask import Flask, request
 import logging
+from datetime import datetime
+
+from flask import Flask, request
 from psycopgwrapper import PG
+from waitress import serve
+
 app = Flask(__name__)
 
 
@@ -10,7 +13,9 @@ app = Flask(__name__)
 def add_sensor_data():
     pg = PG.get_default_postgres()
 
+    # Get request for debugging
     if request.method == 'GET':
+        logging.warning("Debug: GET request")
         packet = {'S1_R1': 500, 'S1_R2': 128000000, 'S1_Voltage': 3.91,
         'S2_R1': 111.1, 'S2_R2': 790123, 'S2_Voltage': 3.64,
         'S3_R1': 135.3, 'S3_R2': 50000000, 'S3_Voltage': 2.39,
@@ -21,78 +26,70 @@ def add_sensor_data():
         'S8_R1': 108.6, 'S8_R2': 373333344, 'S8_Voltage': 4.33,
         'CFG': 0, 'T': 33.5, 'TH': 35.5, 'H': 33.9, 'RH': 32.7,
         'P': 988, 'G': 3172.33, 'IAQ': 56.4, 'CO2': 631.96, 'VOC': 0.82, 'IAC_comp': 3,
-        'timestamp': 1659106708162, 'node_id': 'appa1-mariolino'}
+        'timestamp': 1659106708162, 'node_id': 'appa1-debug'}
+    else:
+        packet = request.json
 
-        # TODO: attrs is a n
+    node_id = pg.fetchone("select id from node where node_name = %s", [packet["node_id"]])
+    sensor_ts = datetime.fromtimestamp(packet["timestamp"] / 1000.0)
 
-        node_id = pg.fetchone("select id from node where node_name = %s", [packet["node_id"]])
+    # TODO: add attrs that do not "key.startswith("S") and key.endswith(("_R1", "_R2", "_Voltage"))"
+    attrs = json.dumps({})
+    packet_id = pg.fetchone("insert into packet(node_id, sensor_ts, attrs) values (%s, %s, %s) returning id", [node_id, sensor_ts, attrs])
+    sensor_ids = pg.fetchall("select id, name from sensor where node_id = %s", [node_id])
+    
+    for sensor_id, sensor_name in sensor_ids:
+        # e.g. sensor_id = 3
+        # e.g. sensor_name: S3_ID
+        pg.upsert("packet_data", {
+            "packet_id": packet_id,
+            "sensor_id": sensor_id,
+            "r1": packet[f"S{sensor_id}_R1"],
+            "r2": packet[f"S{sensor_id}_R2"],
+            "volt": packet[f"S{sensor_id}_Voltage"],
+        }, ["packet_id", "sensor_id"])
 
-        # pg.upsert("packet", {
-        #             "node_id": node_id,
-        #             "sensor_ts": datetime.fromtimestamp(packet["timestamp"] / 1000.0), # TODO: convert into right datetime format
-        #             # "attrs": ... , 
-        #         }, [])
+    pg.close()
+    logging.warning(f"{request.method} request for /sensordata {packet['node_id']}")
 
-        # sensor_ids = pg.fetchall("select id, name from sensor where node_id = %s", [node_id])
-        sensor_ids = pg.fetchall("select id, name from sensor where node_id = %s", [node_id])
-        sensor_ts = datetime.fromtimestamp(packet["timestamp"] / 1000.0)
-
-        # TODO: add attrs that do not "key.startswith("S") and key.endswith(("_R1", "_R2", "_Voltage"))"
-        attrs = json.dumps({})
-        packet_id = pg.fetchone("insert into packet(node_id, sensor_ts) values (%s, %s, %s) returning id", [node_id, sensor_ts, attrs])
-        
-        print("SENSORS IDS", sensor_ids)
-
-
-        # # for key, value in packet.values():
-        # for i in range(1, 9):
-        #     pg.upsert("packet_data", {
-        #         "packet_id": packet_id,
-        #         "sensor_id": sensor_id,
-        #         "r1": packet[f"R{i}_R1"],
-        #         "r2": packet[f"R{i}_R2"],
-        #         "volt": packet[f"R{i}_Voltage"],
-        #     }, ["packet_id", "sensor_id"])
-
-        # Name: e.g. S1_ID ??
-        return "Hello sensor!"
-
-    content = request.json
-    logging.warning(content)
-    return content
+    return packet
 
 
 @app.route('/nodeinfo', methods=['GET', 'POST'])
 def add_node_info():
     pg = PG.get_default_postgres()
 
+    # Get request for debugging
     if request.method == 'GET':
-        mariolino1 = {'node_id': 'appa1-mariolino', 'lat': 46.062934, 'lon': 11.126217,
+        logging.warning("Debug: GET request")
+        content = {'node_id': 'appa1-debug', 'lat': 46.062934, 'lon': 11.126217,
          'description': 'Appa 1 - S. Chiara',
          'S1_ID': 'SnO2_c_MH20_L2', 'S2_ID': 'LaFeO3_1-2021_MH17_L2',
          'S3_ID': 'WO3_1-2021_MH17_L5', 'S4_ID': 'WO3_1-2021_MH17_L5',
          'S5_ID': 'ZnOR_2018_MH17_L2', 'S6_ID': 'ZnOR_2018_MH17_L2',
          'S7_ID': 'SmFeO3_1-2021_MH20_L2', 'S8_ID': 'STN_2018_MH20_L2'}
-        pg.upsert("node", {
-            "node_name": mariolino1["node_id"],
-            "lat": mariolino1["lat"],
-            "lon": mariolino1["lon"],
-            "description": mariolino1["description"]
-        }, ["node_name"])
-        node_id = pg.fetchone("select id from node where node_name=%s", [mariolino1["node_id"]])
-        for key, val in mariolino1.items():
-            if key.startswith("S") and key.endswith("_ID"):
-                pg.upsert("sensor", {
-                    "node_id": node_id,
-                    "name": key,
-                    "description": val,
-                }, ["node_id", "name"])
-        pg.close()
-        return "Hello node! /nodeinfo called"
-    content = request.json
-    logging.warning(content)
+    else:
+        content = request.json
+
+    pg.upsert("node", {
+        "node_name": content["node_id"],
+        "lat": content["lat"],
+        "lon": content["lon"],
+        "description": content["description"]
+    }, ["node_name"])
+    node_id = pg.fetchone("select id from node where node_name=%s", [content["node_id"]])
+    for key, val in content.items():
+        if key.startswith("S") and key.endswith("_ID"):
+            pg.upsert("sensor", {
+                "node_id": node_id,
+                "name": key,
+                "description": val,
+            }, ["node_id", "name"])
+    pg.close()
+    logging.warning(f"{request.method} request for /nodeinfo {content['node_id']}")
     return content
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    # app.run(host='0.0.0.0', debug=True)
+    serve(app, host="0.0.0.0", port=5000)
