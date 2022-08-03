988173044d8ba9ef61a67d24249a7182282e6b30
Davide <dmr475@gmail.com>
2022-08-03 16:42:02 +0200

    add update_db function

--- data_uploader.py
+++ data_uploader.py
@@ -20,6 +20,9 @@ FORMAT    = "csv"
 STATIONS  = "2,4,6,8,9,15,22,23"
 appa_data_url = f"https://bollettino.appa.tn.it/aria/opendata/{FORMAT}"
 
+# update data
+DAYS_TO_FETCH = 7
+
 # DATABASE INITAL POPULATOR
 def populate_db(base_url: str) -> None:
     """
@@ -93,13 +96,13 @@ def populate_db(base_url: str) -> None:
 
         cur.execute(
             "INSERT INTO appa_data (" +
-                    "stazione, " +
-                    "inquinante, " +
-                    "ts, " +
-                    "valore" +
-                ") VALUES " +
-                    insert_data +
-                " ON CONFLICT DO NOTHING" # ignore the insert if the key (stazione, inquinante, ts) already exists
+                "stazione, " +
+                "inquinante, " +
+                "ts, " +
+                "valore" +
+            ") VALUES " +
+                insert_data +
+            " ON CONFLICT DO NOTHING" # ignore the insert if the key (stazione, inquinante, ts) already exists
         )
 
     connection.commit()
@@ -109,4 +112,79 @@ def populate_db(base_url: str) -> None:
         time_delta = time() - population_start
         print(f"finished ({time_delta // 60}m {time_delta % 60}s)")
 
-populate_db(appa_data_url)
+def update_db(base_url: str) -> None:
+    """
+    Updates the db with the last days data, ignoring duplicates
+
+    Args:
+        base_url (str): the starting url
+    """
+    if DEBUG: population_start = time()
+
+    date_start  = date.today()
+    date_end    = date.today() - timedelta(days = DAYS_TO_FETCH)
+    date_string = date_end.strftime("%Y-%m-%d") + "," + date_start.strftime("%Y-%m-%d")
+
+    base_url = base_url + f"/{date_string}/{STATIONS}"
+
+    connection = sql.connect(
+        database = DB_NAME,
+        host = DB_ADDRESS,
+        user = DB_USER,
+        password = DB_PASSWORD
+    )
+    cur = connection.cursor()
+
+    # get the data
+    raw_data = requests.get(base_url).content
+    
+    # convert it into a dataframe
+    temp_df = pd.read_csv(
+        io.StringIO(raw_data.decode("windows 1252"))
+    )
+    
+    # convert all times to string and add 0 before
+    #   single digit numbers to be parsed later
+    temp_df["Ora"] = temp_df["Ora"].astype(str).str.zfill(2)
+
+    # replace 24 hour with 00 so python will recognise it
+    temp_df["Ora"] = temp_df["Ora"].replace("24", "00")
+
+    # replace n.d. values with -1 values
+    temp_df = temp_df.replace("n.d.", -1)
+
+    print(temp_df)
+
+    insert_data = ""
+    for index, row in temp_df.iterrows():
+        # convert date and time in a single object
+        timestamp = datetime.strptime(
+            row['Data'] + ' ' + row['Ora'],
+            "%Y-%m-%d %H"
+        )
+    
+        insert_data += f", ('{row['Stazione']}', '{row['Inquinante']}', '{timestamp.isoformat(' ')}', {row['Valore']})"
+        # timestamp is in ISO format (YYYY-MM-DD) + (HH:MM:SS.mmmmmm) with a space as separator
+    insert_data = insert_data[1:] # remove first ',' to prevent errors
+
+    cur.execute(
+        "INSERT INTO appa_data (" +
+            "stazione, " +
+            "inquinante, " +
+            "ts, " +
+            "valore" +
+        ") VALUES " +
+            insert_data +
+        " ON CONFLICT DO NOTHING" # ignore the insert if the key (stazione, inquinante, ts) already exists
+    )
+
+    connection.commit()
+    connection.close()
+    
+    if DEBUG:
+        time_delta = time() - population_start
+        print(f"finished ({time_delta // 60}m {time_delta % 60}s)")
+
+# script runtime
+#populate_db(appa_data_url)
+update_db(appa_data_url)
