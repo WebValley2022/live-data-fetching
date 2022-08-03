65a5f647bbfc5186c01e3531206e410bbd08400f
Davide <dmr475@gmail.com>
2022-08-03 17:03:28 +0200

    general refactor and optimization of code and add connect_to_db, format_dataframe, build_value_list and execute_insert functions

--- data_uploader.py
+++ data_uploader.py
@@ -23,6 +23,87 @@ appa_data_url = f"https://bollettino.appa.tn.it/aria/opendata/{FORMAT}"
 # update data
 DAYS_TO_FETCH = 7
 
+# UTIL FUNCTIONS
+def connect_to_db(): # return type should be sql.connection but gives error
+    """
+    Connects to the db
+
+    Returns:
+        sql.connection: the connection object related to the db
+    """
+    return sql.connect(
+        database = DB_NAME,
+        host = DB_ADDRESS,
+        user = DB_USER,
+        password = DB_PASSWORD
+    )
+
+def format_dataframe(start_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Formats a dataframe, removes the 'n.d.' values, adds leading 0s
+    in front of single digit numbers and converts dates to datetime objs
+
+    Args:
+        start_df (pd.DataFrame): the dataframe to format
+
+    Returns:
+        pd.DataFrame: the final formatted dataframe
+    """
+    # convert all times to string and add 0 before
+    #   single digit numbers to be parsed later
+    start_df["Ora"] = start_df["Ora"].astype(str).str.zfill(2)
+    
+    # replace 24 hour with 00 so python will recognise it
+    start_df["Ora"] = start_df["Ora"].replace("24", "00")
+    
+    # replace n.d. values with -1 values
+    start_df = start_df.replace("n.d.", -1)
+
+    return start_df
+
+def build_value_list(dataframe: pd.DataFrame) -> str:
+    """
+    Builds the value list to be put in an INSERT sql statement
+
+    Args:
+        dataframe (pd.DataFrame): the dataframe where to extract the data
+
+    Returns:
+        str: the string to be put as values
+    """
+    output = ""
+    for _, row in dataframe.iterrows(): # _ is index
+        # convert date and time in a single object
+        timestamp = datetime.strptime(
+            row['Data'] + ' ' + row['Ora'],
+            "%Y-%m-%d %H"
+        )
+    
+        output += f", ('{row['Stazione']}', '{row['Inquinante']}', '{timestamp.isoformat(' ')}', {row['Valore']})"
+        # timestamp is in ISO format (YYYY-MM-DD) + (HH:MM:SS.mmmmmm) with a space as separator
+    output = output[1:] # remove first ',' to prevent errors
+
+    return output
+
+def execute_insert(cur, insert_data: str) -> None: # cur type should be sql.cursor but gives error
+    """
+    Executes the INSERT sql query with the insert_data given
+
+    Args:
+        cur (sql.cursor): the cursor object where to make the operation
+        insert_data (str): the data to insert
+    """
+    cur.execute(
+        "INSERT INTO appa_data (" +
+            "stazione, " +
+            "inquinante, " +
+            "ts, " +
+            "valore" +
+        ") VALUES " +
+            insert_data +
+        " ON CONFLICT DO NOTHING" # ignore the insert if the key (stazione, inquinante, ts) already exists
+    )
+
 # DATABASE INITAL POPULATOR
 def populate_db(base_url: str) -> None:
     """
@@ -31,15 +112,9 @@ def populate_db(base_url: str) -> None:
     Args:
         base_url (str): the starting url
     """
-
     if DEBUG: population_start = time()
 
-    connection = sql.connect(
-        database = DB_NAME,
-        host = DB_ADDRESS,
-        user = DB_USER,
-        password = DB_PASSWORD
-    )
+    connection = connect_to_db()
     cur = connection.cursor()
 
     i = 0
@@ -68,42 +143,17 @@ def populate_db(base_url: str) -> None:
         # increment index to be used in next cycle
         i += 1
 
-        # convert all times to string and add 0 before
-        #   single digit numbers to be parsed later
-        temp_df["Ora"] = temp_df["Ora"].astype(str).str.zfill(2)
+        # correct some values in dataframe
+        temp_df = format_dataframe(temp_df)
 
-        # replace 24 hour with 00 so python will recognise it
-        temp_df["Ora"] = temp_df["Ora"].replace("24", "00")
-
-        # replace n.d. values with -1 values
-        temp_df = temp_df.replace("n.d.", -1)
-
-        insert_data = ""
-        for index, row in temp_df.iterrows():
-            # convert date and time in a single object
-            timestamp = datetime.strptime(
-                row['Data'] + ' ' + row['Ora'],
-                "%Y-%m-%d %H"
-            )
-    
-            insert_data += f", ('{row['Stazione']}', '{row['Inquinante']}', '{timestamp.isoformat(' ')}', {row['Valore']})"
-            # timestamp is in ISO format (YYYY-MM-DD) + (HH:MM:SS.mmmmmm) with a space as separator
-        insert_data = insert_data[1:] # remove first ',' to prevent errors
+        # build the value to insert into the db
+        insert_data = build_value_list(temp_df)
 
         if DEBUG:
             time_delta = time() - fetch_start
             print(f"fetch finished ({time_delta // 60}m {time_delta % 60}s)")
 
-        cur.execute(
-            "INSERT INTO appa_data (" +
-                "stazione, " +
-                "inquinante, " +
-                "ts, " +
-                "valore" +
-            ") VALUES " +
-                insert_data +
-            " ON CONFLICT DO NOTHING" # ignore the insert if the key (stazione, inquinante, ts) already exists
-        )
+        execute_insert(cur, insert_data)
 
     connection.commit()
     connection.close()
@@ -119,20 +169,17 @@ def update_db(base_url: str) -> None:
     Args:
         base_url (str): the starting url
     """
-    if DEBUG: population_start = time()
-
     date_start  = date.today()
     date_end    = date.today() - timedelta(days = DAYS_TO_FETCH)
     date_string = date_end.strftime("%Y-%m-%d") + "," + date_start.strftime("%Y-%m-%d")
 
+    if DEBUG:
+        print(f"fetching data segment: {date_string}")
+        population_start = time()
+
     base_url = base_url + f"/{date_string}/{STATIONS}"
 
-    connection = sql.connect(
-        database = DB_NAME,
-        host = DB_ADDRESS,
-        user = DB_USER,
-        password = DB_PASSWORD
-    )
+    connection = connect_to_db()
     cur = connection.cursor()
 
     # get the data
@@ -142,41 +189,14 @@ def update_db(base_url: str) -> None:
     temp_df = pd.read_csv(
         io.StringIO(raw_data.decode("windows 1252"))
     )
-    
-    # convert all times to string and add 0 before
-    #   single digit numbers to be parsed later
-    temp_df["Ora"] = temp_df["Ora"].astype(str).str.zfill(2)
 
-    # replace 24 hour with 00 so python will recognise it
-    temp_df["Ora"] = temp_df["Ora"].replace("24", "00")
+    # correct some values in dataframe
+    temp_df = format_dataframe(temp_df)
 
-    # replace n.d. values with -1 values
-    temp_df = temp_df.replace("n.d.", -1)
+    # build the value to insert into the db
+    insert_data = build_value_list(temp_df)
 
-    print(temp_df)
-
-    insert_data = ""
-    for index, row in temp_df.iterrows():
-        # convert date and time in a single object
-        timestamp = datetime.strptime(
-            row['Data'] + ' ' + row['Ora'],
-            "%Y-%m-%d %H"
-        )
-    
-        insert_data += f", ('{row['Stazione']}', '{row['Inquinante']}', '{timestamp.isoformat(' ')}', {row['Valore']})"
-        # timestamp is in ISO format (YYYY-MM-DD) + (HH:MM:SS.mmmmmm) with a space as separator
-    insert_data = insert_data[1:] # remove first ',' to prevent errors
-
-    cur.execute(
-        "INSERT INTO appa_data (" +
-            "stazione, " +
-            "inquinante, " +
-            "ts, " +
-            "valore" +
-        ") VALUES " +
-            insert_data +
-        " ON CONFLICT DO NOTHING" # ignore the insert if the key (stazione, inquinante, ts) already exists
-    )
+    execute_insert(cur, insert_data)
 
     connection.commit()
     connection.close()
@@ -186,5 +206,6 @@ def update_db(base_url: str) -> None:
         print(f"finished ({time_delta // 60}m {time_delta % 60}s)")
 
 # script runtime
-#populate_db(appa_data_url)
-update_db(appa_data_url)
+if __name__ == "__main__":
+    #populate_db(appa_data_url)
+    update_db(appa_data_url)
